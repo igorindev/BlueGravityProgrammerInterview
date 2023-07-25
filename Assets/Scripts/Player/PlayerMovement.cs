9dffd826f6844e7557b982cb8268a2a339127f01
Igor Lamounier <igorindev@gmail.com>
2023-07-25 18:03:42 -0300

    Update player movement to support multiple direction

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,12 +4,15 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float playerSpeed = 5.0f;
     [SerializeField] Animator animator;
+    [SerializeField] Transform mesh;
 
     Rigidbody2D playerRigidbody;
 
     readonly int animSpeed = Animator.StringToHash("Speed");
 
     Vector2 movementInput;
+
+    bool movingRight;
 
     void Start()
     {
@@ -20,11 +23,33 @@ public class PlayerMovement : MonoBehaviour
     {
         MovePlayer();
         animator.SetFloat(animSpeed, playerRigidbody.velocity.magnitude);
+        RotateCharacterToDirection();
     }
 
     void MovePlayer()
     {
         playerRigidbody.velocity = movementInput.normalized * playerSpeed;
+    }
+
+    void RotateCharacterToDirection()
+    {
+        if (movementInput.x < 0 && movingRight == false)
+        {
+            movingRight = true;
+            Invert();
+        }
+        else if (movementInput.x > 0 && movingRight == true)
+        {
+            movingRight = false;
+            Invert();
+        }
+
+        void Invert()
+        {
+            Vector3 scale = mesh.localScale;
+            scale.x *= -1;
+            mesh.localScale = scale;
+        }
     }
 
     public void SendInput(Vector2 input)
