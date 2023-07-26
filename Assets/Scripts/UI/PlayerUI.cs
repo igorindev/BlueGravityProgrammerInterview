0cb27e5aa91e76c4c1b6b4b86549ad46b965125e
Igor Lamounier <igorindev@gmail.com>
2023-07-26 05:17:16 -0300

    Add complementary info to money UI

--- Assets/Scripts/UI/PlayerUI.cs
+++ Assets/Scripts/UI/PlayerUI.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 public class PlayerUI : GameUI
 {
     [SerializeField] TextMeshProUGUI moneyValue;
-    [SerializeField] TextMeshProUGUI dayTimeValue;
 
     public void UpdateMoney(int playerMoney)
     {
-        moneyValue.text = playerMoney.ToString();
-    }
-
-    public void UpdateDayTime(float dayTime)
-    {
-        dayTimeValue.text = dayTime.ToString();
+        moneyValue.text = playerMoney.ToString() + " <color=yellow>$</color>";
     }
 }
